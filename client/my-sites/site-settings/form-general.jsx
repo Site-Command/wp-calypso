48d822f7234621f5b0f6ce117f58b6458058d9e2
Marco Ciampini <marco.ciampini@automattic.com>
2020-12-17 13:10:59 +0100

    Site settings: disable email check for launch section

--- client/my-sites/site-settings/form-general.jsx
+++ client/my-sites/site-settings/form-general.jsx
@@ -12,7 +12,6 @@ import { flowRight, get, has } from 'lodash';
  */
 import wrapSettingsForm from './wrap-settings-form';
 import { Card, CompactCard, Button } from '@automattic/components';
-import EmailVerificationGate from 'calypso/components/email-verification/email-verification-gate';
 import Notice from 'calypso/components/notice';
 import NoticeAction from 'calypso/components/notice/notice-action';
 import LanguagePicker from 'calypso/components/language-picker';
@@ -700,15 +699,6 @@ export class SiteSettingsFormGeneral extends Component {
 
 	privacySettingsWrapper() {
 		if ( this.props.isUnlaunchedSite ) {
-			if ( this.props.needsVerification ) {
-				return (
-					<EmailVerificationGate>
-						{ this.renderLaunchSite() }
-						{ this.privacySettings() }
-					</EmailVerificationGate>
-				);
-			}
-
 			return (
 				<>
 					{ this.renderLaunchSite() }
